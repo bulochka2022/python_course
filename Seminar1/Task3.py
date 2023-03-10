32e273472b6e58d7f8a6762e4de8e5bb25236af1
Tanya <tanya-v_92@mail.ru>
2023-03-10 10:23:22 +0300

    add changes for tasks 1,2,3,4

--- Seminar1/Task3.py
+++ Seminar1/Task3.py
@@ -2,6 +2,8 @@
 Найдите сумму чисел n + nn + nnn."""
 
 
-n = int(input("Введите число: "))
-res = n + n*n + n*n*n
-print(f"Сумма чисел n + nn + nnn равна {res}")
+
+n = input("Введите число n: ")
+res = int(n) + int(n * n) + int(n * n * n)
+print(f'Сумма чисел n + nn + nnn равна {res}')
+
